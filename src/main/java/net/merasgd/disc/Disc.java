94a774f8d3b5639980972d0b6021b6e79c6a5260
MerasGD <discordmeras69@gmail.com>
2024-05-19 21:18:02 +0800

    Changes to be committed: 	modified:   build.gradle 	deleted:    src/client/java/com/example/ExampleModClient.java 	deleted:    src/client/java/com/example/mixin/client/ExampleClientMixin.java 	deleted:    src/client/resources/modid.client.mixins.json 	deleted:    src/main/java/com/example/ExampleMod.java 	deleted:    src/main/java/com/example/mixin/ExampleMixin.java 	new file:   src/main/java/net/merasgd/disc/Disc.java 	new file:   src/main/resources/assets/disc/icon.png 	deleted:    src/main/resources/assets/modid/icon.png 	modified:   src/main/resources/fabric.mod.json 	deleted:    src/main/resources/modid.mixins.json

---
+++ src/main/java/net/merasgd/disc/Disc.java
@@ -0,0 +1,12 @@
+package net.merasgd.disc;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Disc implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+        
+    }
+
+}
