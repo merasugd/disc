3235704de0bc39b1163979edec7e4f84b5f344f5
MerasGD <discordmeras69@gmail.com>
2024-05-19 21:33:22 +0800

    Changes to be committed: 	modified:   src/main/java/net/merasgd/disc/Disc.java

--- src/main/java/net/merasgd/disc/Disc.java
+++ src/main/java/net/merasgd/disc/Disc.java
@@ -1,12 +1,23 @@
 package net.merasgd.disc;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import net.fabricmc.api.ModInitializer;
 
 public class Disc implements ModInitializer {
+    public static final String MOD_ID = "disc";
+    public static final Logger LOGGER = LoggerFactory.getLogger(Disc.MOD_ID);
 
     @Override
     public void onInitialize() {
-        
+        Disc.LOGGER.info("Initializing...");
+
+
     }
 
+    public void registerItem(String name) {
+
+    }
+    
 }
